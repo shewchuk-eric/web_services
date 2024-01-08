bd2fad32dc9d02c8a1e9bde0eedf0ba1b276ca1b
Eric Shewchuk <eric@thebanyanteam.com>
2024-01-07 18:37:47 -1000

    path changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 // const Writer = require('./routes/writer');
-const Writer = require('../routes/writer');
+const Writer = require('./routes/writer');
 const writer = new Writer();
 const app = express();
 const port = 3000;
